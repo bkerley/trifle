9be528a92d92b9379bc399a81e46631a9027b434
Bryce Kerley <bkerley@brycekerley.net>
2009-03-01 11:59:24 -0500

    initial commit

---
+++ main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Trifle
+//
+//  Created by Bryce Kerley on 3/1/09.
+//  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <RubyCocoa/RBRuntime.h>
+
+int main(int argc, const char *argv[])
+{
+    return RBApplicationMain("rb_main.rb", argc, argv);
+}
