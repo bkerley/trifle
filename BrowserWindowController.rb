1b99a9296a45885e751e027c7eaaafaef4835631
Bryce Kerley <bkerley@brycekerley.net>
2009-03-01 20:55:48 -0500

    url fixup

---
+++ BrowserWindowController.rb
@@ -0,0 +1,31 @@
+#
+#  BrowserWindowController.rb
+#  Trifle
+#
+#  Created by Bryce Kerley on 3/1/09.
+#  Copyright (c) 2009 __MyCompanyName__. All rights reserved.
+#
+
+require 'osx/cocoa'
+
+class BrowserWindowController < OSX::NSWindowController
+	ib_outlet :addressBar, :addressOutput, :webView
+	ib_action :addressEntered
+	
+	def awakeFromNib()
+		@webView.setApplicationNameForUserAgent_("Trifle/http://trifle.qlnk.net/")
+	end
+	
+	def addressEntered(sender)
+		user_address = sender.stringValue.to_s
+		fixed_up_address = fixup_address user_address
+		@addressOutput.setStringValue fixed_up_address
+		@webView.setMainFrameURL_ fixed_up_address
+	end
+	
+	private
+	def fixup_address(address)
+		return address if address =~ /^https?/
+		return "http://#{address}"
+	end
+end
