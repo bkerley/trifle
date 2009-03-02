d918dc20800569cf4c6e0640e5a78be265fed430
Bryce Kerley <bkerley@brycekerley.net>
2009-03-01 21:35:11 -0500

    stick status bar, show title on load

--- BrowserWindowController.rb
+++ BrowserWindowController.rb
@@ -9,17 +9,32 @@
 require 'osx/cocoa'
 
 class BrowserWindowController < OSX::NSWindowController
-	ib_outlet :addressBar, :webView
+	ib_outlet :addressBar, :webView, :statusBar
 	ib_action :addressEntered
 	
 	def awakeFromNib()
+		this_bundle = OSX::NSBundle::bundleForClass self
+		home_page = this_bundle.pathForResource_ofType_ 'home', 'html'
+		
 		@webView.setApplicationNameForUserAgent_("Trifle/http://trifle.qlnk.net/")
+		@webView.setFrameLoadDelegate_ self
+		
+		@webView.setMainFrameURL "file://#{home_page}"
 	end
 	
 	def addressEntered(sender)
 		user_address = sender.stringValue.to_s
 		fixed_up_address = fixup_address user_address
 		@webView.setMainFrameURL fixed_up_address
+	end
+	
+	def webView_didReceiveTitle_forFrame(sender, title, frame)
+		return unless frame == sender.mainFrame
+		sender.window.setTitle title.to_s
+	end
+	
+	def webView_didReceiveIcon_forFrame(sender, image, frame)
+		
 	end
 	
 	private
